4c9cf7e33a6860f42b2f9acb2959e0d96f5bb1c0
Tierney Cyren <hello@bnb.im>
2018-10-31 17:22:13 -0400

    Initial functional commit

---
+++ README.md
@@ -0,0 +1,9 @@
+It's me, Tierney!
+
+# Usage
+Via npx:
+```
+npx bitandbang
+```
+
+Not including how to use it globally because I'm not sure why you'd want this as a global command. That'd be creepy.
---
+++ bin/card.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+var chalk = require("chalk")
+
+var twitter = chalk.cyan("@bitandbang")
+var github = chalk.cyan("@bnb")
+var linkedin = chalk.cyan("@bitandbang")
+var site = chalk.cyan("https://bnb.im")
+var npx = "npx bitandbang"
+
+console.log("")
+console.log(chalk.green("╔══════════════════════════════════════════════════════════════════════╗"))
+console.log(chalk.green("║                                                                      ║"))
+console.log(chalk.green("║  ") + "Tierney Cyren – " + chalk.cyan("bitandbang") + chalk.green("                                          ║"))
+console.log(chalk.green("║                                                                      ║"))
+console.log(chalk.green("║  ") + chalk.bold("Twitter:   ") + twitter + chalk.green("                                              ║"))
+console.log(chalk.green("║  ") + chalk.bold("GitHub:    ") + github + chalk.green("                                                     ║"))
+console.log(chalk.green("║  ") + chalk.bold("LinkedIn:  ") + linkedin + chalk.green("                                              ║"))
+console.log(chalk.green("║  ") + chalk.bold("Web:       ") + site + chalk.green("                                           ║"))
+console.log(chalk.green("║                                                                      ║"))
+console.log(chalk.green("║  ") + chalk.bold("Card: ") + npx + chalk.green("                                                ║"))
+console.log(chalk.green("║                                                                      ║"))
+console.log(chalk.green("╚══════════════════════════════════════════════════════════════════════╝"))
+console.log("")
