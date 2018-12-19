151bfded146df8c42050c61c1eb0981864d1f1f9
Tierney Cyren <cyren@Tierneys-MacBook-Pro.local>
2018-12-19 10:50:29 -0800

    Rewrite to be more clean and less hand-rolled

--- bin/card.js
+++ bin/card.js
@@ -1,24 +1,35 @@
 #!/usr/bin/env node
+// 👆 Used to tell Node.js that this is a CLI tool
 
-var chalk = require("chalk")
+// Pull in our modules
+var chalk = require('chalk')
+var boxen = require('boxen')
 
-var twitter = chalk.cyan("@bitandbang")
-var github = chalk.cyan("@bnb")
-var linkedin = chalk.cyan("@bitandbang")
-var site = chalk.cyan("https://bnb.im")
-var npx = "npx bitandbang"
+// Define options for Boxen
+let options = {
+    padding: 1,
+    margin: 1,
+    borderStyle: 'round'
+}
+// Text + chalk definitions
+var work = chalk.white('Senior Cloud Developer Advocate at Microsoft')
+var twitter = chalk.cyan('https://twitter.com/bitandbang')
+var github = chalk.cyan('https://github.com/bnb')
+var linkedin = chalk.cyan('https://linkedin.com/in/bitandbang')
+var site = chalk.cyan('https://bnb.im')
+var npx = chalk.white('npx bitandbang')
 
-console.log("")
-console.log(chalk.green("╔══════════════════════════════════════════════════════════════════════╗"))
-console.log(chalk.green("║                                                                      ║"))
-console.log(chalk.green("║  ") + "Tierney Cyren – " + chalk.cyan("bitandbang") + chalk.green("                                          ║"))
-console.log(chalk.green("║                                                                      ║"))
-console.log(chalk.green("║  ") + chalk.bold("Twitter:   ") + twitter + chalk.green("                                              ║"))
-console.log(chalk.green("║  ") + chalk.bold("GitHub:    ") + github + chalk.green("                                                     ║"))
-console.log(chalk.green("║  ") + chalk.bold("LinkedIn:  ") + linkedin + chalk.green("                                              ║"))
-console.log(chalk.green("║  ") + chalk.bold("Web:       ") + site + chalk.green("                                           ║"))
-console.log(chalk.green("║                                                                      ║"))
-console.log(chalk.green("║  ") + chalk.bold("Card: ") + npx + chalk.green("                                                ║"))
-console.log(chalk.green("║                                                                      ║"))
-console.log(chalk.green("╚══════════════════════════════════════════════════════════════════════╝"))
-console.log("")
+// Actual strings we're going to output
+var newline = '\n'
+var heading = `${chalk.white('Tierney Cyren /')} ${chalk.cyan('bitandbang')}`
+var working = `${chalk.white.bold('      Work:')}  ${work}`
+var twittering = `${chalk.white.bold('   Twitter:')}  ${twitter}`
+var githubing = `${chalk.white.bold('    GitHub:')}  ${github}`
+var linkedining = `${chalk.white.bold('  LinkedIn:')}  ${linkedin}`
+var webing = `${chalk.white.bold('       Web:')}  ${site}`
+var carding = `${chalk.white.bold('      Card:')}  ${npx}`
+
+let output = heading + newline + newline + working  + newline + twittering + newline + githubing + newline + linkedining + newline + webing  + newline  + newline + carding
+
+
+console.log(chalk.green(boxen(output, options)))
