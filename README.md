c4ca6aeae1ae9231041b04d32d1bf98c3a1ea4bd
Carlos Azaustre <cazaustre@gmail.com>
2018-12-20 13:40:23 +0100

    add own handlers

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
-It's me, Tierney!
+It's me, Carlos!
+> based in (@bitandbang)[https://github.com/bnb/bitandbang] work
 
 # Usage
 Via npx:
 ```
-npx bitandbang
+npx carlosazaustre
 ```
 
 Not including how to use it globally because I'm not sure why you'd want this as a global command. That'd be creepy.
--- bin/card.js
+++ bin/card.js
@@ -9,19 +9,18 @@ var boxen = require('boxen')
 let options = {
   padding: 1,
   margin: 1,
-  borderStyle: 'round'
+  borderStyle: 'double-single'
 }
 // Text + chalk definitions
 let data = {
-  'name': chalk.white('Tierney Cyren /'),
-  'handle': chalk.cyan('bitandbang'),
-  'work': chalk.white('Senior Cloud Developer Advocate at Microsoft'),
-  'twitter': chalk.cyan('https://twitter.com/bitandbang'),
-  'github': chalk.cyan('https://github.com/bnb'),
-  'linkedin': chalk.cyan('https://linkedin.com/in/bitandbang'),
-  'web': chalk.cyan('https://bnb.im'),
-  'npx': chalk.white('npx bitandbang'),
-  'labelWork': chalk.white.bold('      Work:'),
+  'name': chalk.white.bold('Carlos Azaustre'),
+  'handle': chalk.yellow('@carlosazaustre'),
+  'work': chalk.white('👨🏻‍💻 JavaScript Developer\n🎓 BSc in Telematics Engineering'),
+  'twitter': chalk.yellow('https://twitter.com/carlosazaustre'),
+  'github': chalk.yellow('https://github.com/carlosazaustre'),
+  'linkedin': chalk.yellow('https://linkedin.com/in/carlosazaustre'),
+  'web': chalk.yellow('https://carlosazaustre.es'),
+  'npx': chalk.white('npx carlosazaustre'),
   'labelTwitter': chalk.white.bold('   Twitter:'),
   'labelGitHub': chalk.white.bold('    GitHub:'),
   'labelLinkedIn': chalk.white.bold('  LinkedIn:'),
@@ -31,8 +30,8 @@ let data = {
 
 // Actual strings we're going to output
 var newline = '\n'
-var heading = `${data.name} ${data.handle}`
-var working = `${data.labelWork}  ${data.work}`
+var heading = `${data.name} (${data.handle})`
+var working = `${data.work}`
 var twittering = `${data.labelTwitter}  ${data.twitter}`
 var githubing = `${data.labelGitHub}  ${data.github}`
 var linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
@@ -40,6 +39,12 @@ var webing = `${data.labelWeb}  ${data.web}`
 var carding = `${data.labelCard}  ${data.npx}`
 
 // Put all our output together into a single variable so we can use boxen effectively
-let output = heading + newline + newline + working + newline + twittering + newline + githubing + newline + linkedining + newline + webing + newline + newline + carding
+let output = heading + newline +
+  working + newline + newline +
+  twittering + newline +
+  githubing + newline +
+  linkedining + newline +
+  webing + newline + newline +
+  carding
 
-console.log(chalk.green(boxen(output, options)))
+console.log(chalk.yellow(boxen(output, options)))
