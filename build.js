a10b5a94f676553446507c62851f5ea881acda26
Tierney Cyren <hello@bnb.im>
2022-06-08 11:54:35 -0500

    chore: some necessary updates
    Signed-off-by: Tierney Cyren <hello@bnb.im>

--- build.js
+++ build.js
@@ -15,31 +15,28 @@ const options = {
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('               Tierney Cyren'),
-  handle: chalk.white('bitandbang'),
-  work: chalk.white('Senior Cloud Developer Advocate at Microsoft'),
-  opensource: chalk.white('Node.js Community Committee ') + chalk.green('⬢'),
+  name: chalk.white('           Tierney Cyren'),
+  handle: chalk.white('bitandbang / bnb'),
+  work: chalk.white('Staff Developer Advocate at ??????'),
   twitter: chalk.gray('https://twitter.com/') + chalk.cyan('bitandbang'),
   npm: chalk.gray('https://npmjs.com/') + chalk.red('~bnb'),
   github: chalk.gray('https://github.com/') + chalk.green('bnb'),
   linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('bitandbang'),
   web: chalk.cyan('https://bnb.im'),
-  npx: chalk.red('npx') + ' ' + chalk.white('@bnb/card  (via GitHub Package Registry)'),
-  labelWork: chalk.white.bold('       Work:'),
-  labelOpenSource: chalk.white.bold('Open Source:'),
-  labelTwitter: chalk.white.bold('    Twitter:'),
-  labelnpm: chalk.white.bold('        npm:'),
-  labelGitHub: chalk.white.bold('     GitHub:'),
-  labelLinkedIn: chalk.white.bold('   LinkedIn:'),
-  labelWeb: chalk.white.bold('        Web:'),
-  labelCard: chalk.white.bold('       Card:')
+  npx: chalk.red('npx') + ' ' + chalk.white('bitandbang'),
+  labelWork: chalk.white.bold('    Work:'),
+  labelTwitter: chalk.white.bold(' Twitter:'),
+  labelnpm: chalk.white.bold('     npm:'),
+  labelGitHub: chalk.white.bold('  GitHub:'),
+  labelLinkedIn: chalk.white.bold('LinkedIn:'),
+  labelWeb: chalk.white.bold('     Web:'),
+  labelCard: chalk.white.bold('    Card:')
 }
 
 // Actual strings we're going to output
 const newline = '\n'
 const heading = `${data.name} / ${data.handle}`
 const working = `${data.labelWork}  ${data.work}`
-const opensourcing = `${data.labelOpenSource}  ${data.opensource}`
 const twittering = `${data.labelTwitter}  ${data.twitter}`
 const npming = `${data.labelnpm}  ${data.npm}`
 const githubing = `${data.labelGitHub}  ${data.github}`
@@ -51,7 +48,6 @@ const carding = `${data.labelCard}  ${data.npx}`
 const output = heading + // data.name + data.handle
                newline + newline + // Add one whole blank line
                working + newline + // data.labelWork + data.work
-               opensourcing + newline + newline + // data.labelOpenSource + data.opensource
                twittering + newline + // data.labelTwitter + data.twitter
                npming + newline + // data.labelnpm + data.npm
                githubing + newline + // data.labelGitHub + data.github
